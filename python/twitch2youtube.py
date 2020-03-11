80df6f0f1a7b97ab06c32ce9671e2428baff6184
Hagnor <adrien.bourdon@outlook.fr>
2020-03-11 23:40:28 +0100

    v001

--- python/twitch2youtube.py
+++ python/twitch2youtube.py
@@ -1,34 +1,42 @@
 import os
+import numpy as np
 def parseOK(path):
     tab_init=[]
+    tab_all=[]
     nline=0
     nblien=0
     block=0
     with open (path, 'rt') as infile:  
         for line in infile:
             nline=nline+1
-#            print(line)
-#            print(nline)
             if  'lien' in line:
                 if block!=0:
-#                    print("Input file error")
+                    print("Input file error")
                     exit()
                 temp=(line.split('='))[1].rstrip()
                 tab_init.append(temp)
                 block=nline
                 nblien=nblien+1
-#                print(block)
             if 'mode' in line:
                 if block==0:
-#                    print("Input file error")
+                    print("Input file error")
                     exit()
                 temp=(line.split('='))[1].rstrip()
                 temp1=nline-(block)
-                tab_init.append(temp1-1)            
+                tab_init.append(temp1-2)            
                 tab_init.append(temp)
+                tab_all.append(tab_init)
+                tab_init=[]
                 block=0
+            if 'nom' in line:
+                if block==0:
+                    print("Input file error")
+                    exit()
+                temp=(line.split('='))[1].rstrip()
+                tab_init.append(temp)
     tab_init.insert(0,nblien)
-    return tab_init   
+    print(tab_all)
+    return tab_init
+            
 link="time.txt"
 firstparse=parseOK(link)
-print(firstparse)
